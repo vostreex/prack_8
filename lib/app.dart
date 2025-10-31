c16619b5ce6603478425d203ea74af88813d3e04
vostreex <vostan97@gmail.com>
2025-10-31 16:11:53 +0300

    Integration into MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:prack_8/app_router.dart';
+import 'data/note_repository.dart';
+import 'features/notes/widgets/note_inherited.dart';
 import 'shared/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-
   final GoRouter _router = AppRouter().router;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      theme: AppTheme.lightTheme,
+    final repo = NoteRepository();  // Создание единственного экземпляра репозитория
+    return NoteInherited(
+      repository: repo,
+      child: MaterialApp.router(
+        routerConfig: _router,
+        theme: AppTheme.lightTheme,
+      ),
     );
   }
 }
